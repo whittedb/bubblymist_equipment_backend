7249ae54e1d1f2bf28d9bd29aa513db6bfe629da
Brad Whitted <whittedb@the-zoo.net>
2020-05-13 06:08:32 -0500

    - Fix typo in input file of create_env_file.py

--- create_env_file.py
+++ create_env_file.py
@@ -1,17 +1,14 @@
 import re
-from pathlib import Path
 
 envre = re.compile(r'''^([^\s=]+)=(?:[\s"']*)(.+?)(?:[\s"']*)$''')
 result = {}
 
-location = Path("../environments")
-file_to_open = location / "bme_equipment_backend"
-with open(file_to_open) as ins:
+with open("bm_equipment_backend") as ins:
     for line in ins:
         match = envre.match(line)
         if match is not None:
             result[match.group(1)] = match.group(2)
 
-with open("environment") as out:
-    for k, v in result:
-        out.write("export set {}={}".format(k, v))
+with open("environment", mode="w") as out:
+    for k, v in result.items():
+        out.write("export set {}={}\n".format(k, v))
